8ced6e5b3c6f525d3681738b2a11de69f29675e7
Pratik <planetpratik@live.in>
2018-07-08 21:46:57 +0530

    feat: Implement 'Add Question' functionality
    Implement 'add new poll' function and redirect to root upon completion.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,32 @@
+import { _saveQuestion } from '../utils/_DATA'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function receiveQuestions(questions) {
     return {
       type: RECEIVE_QUESTIONS,
-      questions,
-    };
+      questions
+    }
+  }
+
+  function addQuestion(question) {
+    return {
+      type: ADD_QUESTION,
+      question
+    }
+  }
+
+  export function handleAddQuestion({optionOneText, optionTwoText,authedUser}) {
+    return (dispatch) => {
+      dispatch(showLoading())
+      return _saveQuestion({
+        optionOneText,
+        optionTwoText,
+        author:authedUser
+      }).then((question) =>
+        dispatch(addQuestion(question))
+        ).then(() =>dispatch(hideLoading()))
+    }
   }
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,13 +1,61 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { Card, Input, Divider, Button } from 'semantic-ui-react'
+import { handleAddQuestion } from '../actions/questions';
 
 class NewQuestion extends Component {
+    state = {
+        optionOneText:'',
+        optionTwoText:''
+    }
+     handleSubmit = (e) => {
+        e.preventDefault()
+        const {dispatch, authedUser,history} = this.props
+        const {optionOneText,optionTwoText} = this.state
+
+        if(optionOneText&&optionTwoText) {
+            dispatch(handleAddQuestion({optionOneText,optionTwoText,authedUser}))
+        }
+        history.push('/')
+    }
+
+    handleInputOneChange = (e, data) => {
+        e.preventDefault()
+        this.setState({
+            optionOneText:data.value
+        })
+    }
+
+    handleInputTwoChange = (e, data) => {
+        e.preventDefault()
+        this.setState({
+            optionTwoText:data.value
+        })
+    }
+
     render() {
         return(
-            <div>
-                NewQuestion
+            <div className='add-new-question-form'>
+                <Card fluid>
+                    <Card.Header  textAlign='center' style={{ marginTop:7.5, marginBottom:0,height:40, fontSize:25}} className='ui header'>Create New Question</Card.Header>
+                    <Card.Content>
+                        <Card.Description >Complete the question:</Card.Description>
+                        <Card.Header  style={{marginTop:'20px', marginBottom:'10px'}}>Would you rather ...</Card.Header>
+                        <Input fluid placeholder='Enter Option One Text Here' style={{marginTop:'20px'}}  onChange={this.handleInputOneChange}></Input>
+                        <Divider horizontal>OR</Divider>
+                        <Input fluid placeholder='Enter Option Two Text Here' onChange={this.handleInputTwoChange}></Input>
+                        <Button fluid color='teal' style={{ justifyContent:'center',marginTop:'20px', marginBottom:'10px'}} onClick={this.handleSubmit}>Submit</Button>
+                    </Card.Content>
+                </Card>
             </div>
         )
     }
 }
 
-export default NewQuestion
+function mapStateToProps({authedUser}) {
+    return {
+        authedUser
+    }
+}
+
+export default connect(mapStateToProps)(NewQuestion)
